3859b6e745953fe5759f464f696d42cc72a95aac
Даниил Хохлов <xoxloff.daniil72@yandex.ru>
2020-08-28 23:26:40 +0500

    Pushing project to master

---
+++ Assets/Scripts/Mill.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Mill : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject millBlades;
+
+    void Update()
+    {
+        millBlades.transform.Rotate(new Vector3(-100*Time.deltaTime, 0, 0), Space.Self);
+    }
+}
